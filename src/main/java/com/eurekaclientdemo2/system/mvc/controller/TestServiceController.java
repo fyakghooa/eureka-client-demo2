a6371d3ff09969fcf42ef7c658cea85b18eabdb1
shawn <shawn@tradeserv.com>
2021-04-18 15:24:53 +0800

    add ribbon

--- src/main/java/com/eurekaclientdemo2/system/mvc/controller/TestServiceController.java
+++ src/main/java/com/eurekaclientdemo2/system/mvc/controller/TestServiceController.java
@@ -1,5 +1,6 @@
 package com.eurekaclientdemo2.system.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +14,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RequestMapping("test2")
 public class TestServiceController {
 
+	@Value("${eureka.instance.instance-id}")
+	private String instanceId; // 注入application.properties的eureka.instance.instance-id的值
+
 	@GetMapping(value = "/{message}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String getAllMessagesByMemberId(@PathVariable String message) throws JsonProcessingException {
-		String messages = "finish call  {" + message + "} in test2 service";
+		String messages = "finish call  {" + message + "} in test2 service , instance id : " + instanceId;
 		return new ObjectMapper().writeValueAsString(messages);
 	}
 
